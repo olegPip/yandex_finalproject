0fbdd611785b39aaa090507a3e79af39402e2b17
olegPip <olegpipuli@mail.ru>
2024-08-21 11:07:37 +0300

    nem

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Олег Цыбулин, 20-я когорта — Финальный проект. Инженер по тестированию плюс
+
+URL_SERVICE = " https://f02515cd-73e8-4920-a828-851c995dde6a.serverhub.praktikum-services.ru"
+CREAT_ORDERS = "/api/v1/orders"
